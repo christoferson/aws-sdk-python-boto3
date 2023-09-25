b11f1fc800277ba79a9f83456b3cf4c5360e2cb0
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-09-25 23:17:04 +0900

    Add run_demo_text_in_image

--- demo_rekognition.py
+++ demo_rekognition.py
@@ -5,7 +5,9 @@ def run_demo(session):
 
     rekognition = session.client("rekognition")
 
-    run_demo_detect_labels(rekognition)
+    #run_demo_detect_labels(rekognition)
+
+    run_demo_text_in_image(rekognition)
 
 
 def run_demo_detect_labels(rekognition):
@@ -18,3 +20,14 @@ def run_demo_detect_labels(rekognition):
     print('Detected labels for ' + key)
     for label in response['Labels']:
         print (label['Name'] + ' : ' + str(label['Confidence']))
+
+def run_demo_text_in_image(rekognition):
+
+    bucket = config.rekognition['bucket']
+    key = config.rekognition['object2']
+
+    response = rekognition.detect_text(Image={'S3Object':{'Bucket': bucket, 'Name': key }})
+
+    print('Detected labels for ' + key)
+    for label in response['TextDetections']:
+        print (label['DetectedText'] + ' : ' + str(round(float(label['Confidence']), 2)))
