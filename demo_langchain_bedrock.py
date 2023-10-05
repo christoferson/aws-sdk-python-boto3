34afab73622a90f2e3d22014a362d900addcecce
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-10-06 00:22:56 +0900

    langchain bedrock

--- demo_langchain_bedrock.py
+++ demo_langchain_bedrock.py
@@ -1,4 +1,9 @@
 from langchain.llms import Bedrock
+from langchain.chat_models import BedrockChat
+from langchain.schema import HumanMessage
+
+# ValueError: Error raised by bedrock service: An error occurred (AccessDeniedException) when calling the InvokeModel operation: Your account is not authorized to invoke this API operation.
+# NotImplementedError: Provider ai21 model does not support chat.
 
 def run_demo(session):
 
@@ -7,21 +12,36 @@ def run_demo(session):
     bedrock_runtime = session.client('bedrock-runtime', region_name="us-east-1")
 
     model_id = "ai21.j2-mid-v1"
+    #model_id = "anthropic.claude-v1"
+    model_kwargs = { "temperature": 0.0 }
+    prompt = "What is the diameter of Earth?"
 
-    #llm = Bedrock(
-        #credentials_profile_name = "profile",
-        #model_id = model_id
-    #)
+    demo_invoke_model_predict(bedrock_runtime, model_id, model_kwargs, prompt)
+
+    demo_invoke_model_chat(bedrock_runtime, model_id, model_kwargs, prompt)
+
+def demo_invoke_model_predict(bedrock_runtime, model_id, model_kwargs, prompt):
 
     llm = Bedrock(
         client = bedrock_runtime,
         model_id = model_id,
-        model_kwargs = {"temperature": 0.0},
+        model_kwargs = model_kwargs,
     )
 
-    demo_invoke_model_predict(llm, model_id, "What is the diameter of Earth?")
-
-def demo_invoke_model_predict(llm, model_id, prompt):
-
     output = llm.predict(prompt)
+
     print(output)
+
+def demo_invoke_model_chat(bedrock_runtime, model_id, model_kwargs, prompt):
+
+    llm = BedrockChat(
+        client = bedrock_runtime,
+        model_id = model_id,
+        model_kwargs = model_kwargs,
+    )
+
+    result = llm([
+        HumanMessage(content = prompt)
+    ])
+
+    print(result.content)
