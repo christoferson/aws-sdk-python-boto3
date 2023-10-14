3814e4efa0c3adce8f3133f56404fb36002dc7e9
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-10-15 00:25:57 +0900

    refactor utils

--- demo_bedrock.py
+++ demo_bedrock.py
@@ -16,13 +16,8 @@ def run_demo(session):
 
     model_id = "amazon.titan-embed-text-v1"
 
-    val1 = demo_embedding_calculate(bedrock_runtime, model_id, "埋め込みの実験のためのサンプルテキストです")
-    val2 = demo_embedding_calculate(bedrock_runtime, model_id, "ぺぺろんちに食べたい")
-    val3 = demo_embedding_calculate(bedrock_runtime, model_id, "This is an example text for testing embeddings.")
+    demo_embedding_calculate_with_cosine_similarity(bedrock_runtime, model_id)
 
-    print(cosine_similarity(val1, val2))
-    print(cosine_similarity(val2, val3))
-    print(cosine_similarity(val1, val3))
 
 def demo_list_foundation_models(bedrock):
 
@@ -64,5 +59,12 @@ def demo_embedding_calculate(bedrock_runtime, model_id, prompt):
 
     return embedding
 
-def cosine_similarity(v1, v2):
-    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+def demo_embedding_calculate_with_cosine_similarity(bedrock_runtime, model_id):
+
+    val1 = demo_embedding_calculate(bedrock_runtime, model_id, "埋め込みの実験のためのサンプルテキストです")
+    val2 = demo_embedding_calculate(bedrock_runtime, model_id, "ぺぺろんちに食べたい")
+    val3 = demo_embedding_calculate(bedrock_runtime, model_id, "This is an example text for testing embeddings.")
+
+    print(cmn_utils.cosine_similarity(val1, val2))
+    print(cmn_utils.cosine_similarity(val2, val3))
+    print(cmn_utils.cosine_similarity(val1, val3))
