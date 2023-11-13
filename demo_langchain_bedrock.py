12f9cfead7b36d814f4d00ed88247f311e93e73e
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-11-13 23:15:06 +0900

    Add demo_embeddings_peek_vector_db

--- demo_langchain_bedrock.py
+++ demo_langchain_bedrock.py
@@ -120,3 +120,95 @@ def demo_embeddings_create_vector_db(bedrock_runtime,
     print(f"Vector Database Initialized. {VECTOR_DB_PATH}")
 
     return db
+
+    
+def demo_embeddings_load_vector_db(bedrock_runtime, model_id='amazon.titan-embed-text-v1', vector_db_collection_name='ec2_faq', vector_db_path='embedding/vectordb/qdrant'):
+
+    print("Call demo_embeddings_load_vector_db")
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    VECTOR_DB_PATH = os.path.join(ROOT_DIR, vector_db_path)
+    print("VECTOR_DB_PATH: " + VECTOR_DB_PATH)
+
+    client = qdrant_client.QdrantClient(
+        path=VECTOR_DB_PATH, prefer_grpc=True
+    )
+
+    embeddings = BedrockEmbeddings(
+        client=bedrock_runtime,
+        model_id=model_id
+    )
+
+    db = Qdrant(
+        client=client, collection_name=vector_db_collection_name, 
+        embeddings=embeddings
+    )
+
+    print(f"Loaded Vector Database. {VECTOR_DB_PATH}")
+
+    return db
+
+def demo_embeddings_peek_vector_db(vectordb, query):
+
+    print("Call demo_embeddings_peek_vector_db")
+
+    #docs = vectordb.similarity_search(query=query, k=10)
+    #for i in docs:
+    #    print({"content": i.page_content[0:20], "metadata": i.metadata})
+
+    docs = vectordb.similarity_search_with_score(query=query, k=10)
+    for i in docs:
+        doc, score = i
+        print({"score": score, "content": doc.page_content[0:20], "metadata": doc.metadata} )
+
+def demo_embeddings_invoke_model_chat(bedrock_runtime, model_id, vectordb, model_kwargs, query):
+
+    print("Call demo_embeddings_invoke_model_chat")
+
+    # 1. Create Prompt Template for Claude
+
+    prompt_template = '''Human: 
+    Text: {context}
+
+    Question: {question}
+
+    Answer the question based on the text provided. If the text doesn't contain the answer, reply that the answer is not available.
+
+    Assistant:
+    '''
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=['context', 'question']
+    )
+
+    # 2. Instantiate Claude Bedrock
+
+    llm = Bedrock(
+        client = bedrock_runtime,
+        model_id = model_id,
+        model_kwargs = model_kwargs,
+        verbose=True
+    )
+
+    # 3. Create RetrievalQA
+
+    chain_type_kwargs = {"prompt": PROMPT}
+
+    qa = RetrievalQA.from_chain_type(llm = llm, 
+                                 chain_type = "stuff", 
+                                 retriever = vectordb.as_retriever(), 
+                                 chain_type_kwargs = chain_type_kwargs, 
+                                 return_source_documents = True)
+    
+    # 4. Invoke
+
+    answer = qa(query)
+
+    #print(answer)
+    print()
+    print(f"Query: {answer['query']}")
+    print(f"Answer: {answer['result']}")
+    print("Source Documents: ")
+    for source_document in answer["source_documents"]:
+        print(f"- {source_document.page_content[0:70]}") #print(f"{source_document.page_content[0:50]} {source_document.metadata}")
+    
