0ffb81c86eb62931e9782389607676ae1d8e41fe
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-12-04 20:39:34 +0900

    Add demo_titan_v1_outpaint

--- demo_bedrock_amz_titan_image.py
+++ demo_bedrock_amz_titan_image.py
@@ -23,9 +23,9 @@ def run_demo(session):
     model_id = "amazon.titan-image-generator-v1:0"
     
     iconfig = config_amz_titan_image.shoe_2B
-    demo_titan_v1_generate_image(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
+    #demo_titan_v1_generate_image(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
     #demo_titan_v1_inpaint(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
-
+    demo_titan_v1_outpaint(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
 ####################
 
 def demo_titan_v1_generate_image(bedrock_runtime, model_id, 
@@ -105,6 +105,7 @@ def demo_titan_v1_generate_image(bedrock_runtime, model_id,
     print("Complete")
 
 
+#########################
 
 def demo_titan_v1_inpaint(bedrock_runtime, model_id, 
                                  prompt : str, 
@@ -191,6 +192,94 @@ def demo_titan_v1_inpaint(bedrock_runtime, model_id,
 
     print("Complete")
 
+
+
+#########################
+
+def demo_titan_v1_outpaint(bedrock_runtime, model_id, 
+                                 prompt : str, 
+                                 negative_prompts : str, 
+                                 quality : str ="standard",
+                                 style_preset="comic-book", 
+                                 cfg_scale : float = 8.0):
+
+    print(f"Call demo_titan_v1_outpaint | style_preset={style_preset} | cfg_scale={cfg_scale}")
+
+    print(f"PROMPT: {prompt}")
+    print(f"NEG_PROMPT: {negative_prompts}")
+
+    ####
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    file_extension = ".png"
+    OUTPUT_IMG_PATH = os.path.join(ROOT_DIR, "titan-image/outpaint/out/{}{}".format(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"), file_extension))
+    print("OUTPUT_IMG_PATH: " + OUTPUT_IMG_PATH)
+
+
+
+    seed = random.randint(0, 214783647)
+    steps = 30 #50
+    start_schedule = 0.6
+    change_prompt = prompt
+    size = 1024
+
+    INPUT_IMG_PATH = "titan-image/outpaint/in/20231203_224811.png"
+    print(f"Loading Reference Image ... {INPUT_IMG_PATH}")
+    input_image_b64 = image_to_base64(Image.open(INPUT_IMG_PATH).resize((size, size)))
+
+    # 
+    config = {
+        "filename": OUTPUT_IMG_PATH,
+        "seed": seed,
+        "change_prompt": change_prompt,
+        "steps": steps,
+        "cfg_scale": cfg_scale,
+        "size": size,
+        "quality": quality,
+        "negative_prompts": negative_prompts
+    }
+
+    # 
+    body = json.dumps(
+        {
+            "taskType": "OUTPAINTING",
+            "outPaintingParams": {
+                "text": "Shoe with a vibrant and lively city background. Do not change the shoe. Shoe and background transition must be seamless and blend well.",
+                "negativeText": negative_prompts,
+                "image": input_image_b64,
+                "maskPrompt": "Shoe Background", #"maskPrompt": "Just change the background. Do not change the shoe.",
+                #"maskImage": input_image_b64 # Only either 'maskPrompt' or 'maskImage' can be provided
+            },
+            "imageGenerationConfig": {
+                "cfgScale": config["cfg_scale"], #8, #Range: 1.0 (exclusive) to 10.0
+                "seed": config["seed"], #Range: 0 to 214783647
+                "quality": quality, #Options: standard/premium
+                "width": 1024,
+                "height": 1024,
+                "numberOfImages": 1, #Range: 1 to 5
+            }
+        }
+    )
+
+    #print(json.dumps(body, indent=2))
+
+    # 
+    print("Generating Image ...")
+    response = bedrock_runtime.invoke_model(body=body, modelId=model_id)
+
+    # 
+    response_body = json.loads(response.get("body").read())
+    #print(response_body.keys())
+    response_image = base64_to_image(response_body["images"][0])
+    #response_image = base64_to_image(response_body["images"][0].get("base64"))
+
+    # 
+    response_image.save(OUTPUT_IMG_PATH)
+    # 
+    with open("{}.json".format(OUTPUT_IMG_PATH), "w") as f:
+        json.dump(config, f, ensure_ascii = False)
+
+    print("Complete")
 
 ### Utilities
 
