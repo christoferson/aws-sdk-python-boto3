ac64ddb9cadc1da1494ade9e8c63a14bab0c6eff
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-12-05 00:35:15 +0900

    Add demo_titan_v1_variation

--- demo_bedrock_amz_titan_image.py
+++ demo_bedrock_amz_titan_image.py
@@ -23,9 +23,12 @@ def run_demo(session):
     model_id = "amazon.titan-image-generator-v1:0"
     
     iconfig = config_amz_titan_image.shoe_2B
-    #demo_titan_v1_generate_image(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
+    demo_titan_v1_generate_image(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
     #demo_titan_v1_inpaint(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
-    demo_titan_v1_outpaint(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
+    #demo_titan_v1_outpaint(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
+    #demo_titan_v1_variation(bedrock_runtime, model_id, iconfig["text"].strip(), iconfig["negative_text"].strip(), iconfig["quality"], iconfig["style"], iconfig["scale"])
+
+
 ####################
 
 def demo_titan_v1_generate_image(bedrock_runtime, model_id, 
@@ -35,7 +38,7 @@ def demo_titan_v1_generate_image(bedrock_runtime, model_id,
                                  style_preset="comic-book", 
                                  cfg_scale : float = 8.0):
 
-    print(f"Call demo_titan_v1_generate_image | style_preset={style_preset} | cfg_scale={cfg_scale}")
+    print(f"Call demo_titan_v1_generate_image | quality={quality} | cfg_scale={cfg_scale}")
 
     print(f"PROMPT: {prompt}")
     print(f"NEG_PROMPT: {negative_prompts}")
@@ -44,7 +47,7 @@ def demo_titan_v1_generate_image(bedrock_runtime, model_id,
 
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     file_extension = ".png"
-    OUTPUT_IMG_PATH = os.path.join(ROOT_DIR, "titan-image/out/{}{}".format(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"), file_extension))
+    OUTPUT_IMG_PATH = os.path.join(ROOT_DIR, "titan-image/generate/out/{}{}".format(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"), file_extension))
     print("OUTPUT_IMG_PATH: " + OUTPUT_IMG_PATH)
 
     seed = random.randint(0, 214783647)
@@ -80,6 +83,7 @@ def demo_titan_v1_generate_image(bedrock_runtime, model_id,
                 "width": 1024,
                 "height": 1024,
                 "numberOfImages": 1, #Range: 1 to 5
+                #"stepSize": 500,
             }
         }
     )
@@ -249,6 +253,93 @@ def demo_titan_v1_outpaint(bedrock_runtime, model_id,
                 "image": input_image_b64,
                 "maskPrompt": "Shoe Background", #"maskPrompt": "Just change the background. Do not change the shoe.",
                 #"maskImage": input_image_b64 # Only either 'maskPrompt' or 'maskImage' can be provided
+            },
+            "imageGenerationConfig": {
+                "cfgScale": config["cfg_scale"], #8, #Range: 1.0 (exclusive) to 10.0
+                "seed": config["seed"], #Range: 0 to 214783647
+                "quality": quality, #Options: standard/premium
+                "width": 1024,
+                "height": 1024,
+                "numberOfImages": 1, #Range: 1 to 5
+            }
+        }
+    )
+
+    #print(json.dumps(body, indent=2))
+
+    # 
+    print("Generating Image ...")
+    response = bedrock_runtime.invoke_model(body=body, modelId=model_id)
+
+    # 
+    response_body = json.loads(response.get("body").read())
+    #print(response_body.keys())
+    response_image = base64_to_image(response_body["images"][0])
+    #response_image = base64_to_image(response_body["images"][0].get("base64"))
+
+    # 
+    response_image.save(OUTPUT_IMG_PATH)
+    # 
+    with open("{}.json".format(OUTPUT_IMG_PATH), "w") as f:
+        json.dump(config, f, ensure_ascii = False)
+
+    print("Complete")
+
+
+
+#########################
+
+def demo_titan_v1_variation(bedrock_runtime, model_id, 
+                                 prompt : str, 
+                                 negative_prompts : str, 
+                                 quality : str ="standard",
+                                 style_preset="comic-book", 
+                                 cfg_scale : float = 8.0):
+
+    print(f"Call demo_titan_v1_variation | style_preset={style_preset} | cfg_scale={cfg_scale}")
+
+    print(f"PROMPT: {prompt}")
+    print(f"NEG_PROMPT: {negative_prompts}")
+
+    ####
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    file_extension = ".png"
+    OUTPUT_IMG_PATH = os.path.join(ROOT_DIR, "titan-image/variation/out/{}{}".format(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"), file_extension))
+    print("OUTPUT_IMG_PATH: " + OUTPUT_IMG_PATH)
+
+
+
+    seed = random.randint(0, 214783647)
+    steps = 30 #50
+    start_schedule = 0.6
+    change_prompt = prompt
+    size = 1024
+
+    INPUT_IMG_PATH = "titan-image/variation/in/20231203_224811.png"
+    print(f"Loading Reference Image ... {INPUT_IMG_PATH}")
+    input_image_b64 = image_to_base64(Image.open(INPUT_IMG_PATH).resize((size, size)))
+
+    # 
+    config = {
+        "filename": OUTPUT_IMG_PATH,
+        "seed": seed,
+        "change_prompt": change_prompt,
+        "steps": steps,
+        "cfg_scale": cfg_scale,
+        "size": size,
+        "quality": quality,
+        "negative_prompts": negative_prompts
+    }
+
+    # 
+    body = json.dumps(
+        {
+            "taskType": "IMAGE_VARIATION",
+            "imageVariationParams": {
+                "text": "Randomize the color of the shoe using contrasting colors, photo-realistic, 8k, hdr",
+                "negativeText": negative_prompts,
+                "images": [ input_image_b64 ],
             },
             "imageGenerationConfig": {
                 "cfgScale": config["cfg_scale"], #8, #Range: 1.0 (exclusive) to 10.0
