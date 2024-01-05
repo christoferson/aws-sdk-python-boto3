559a376c3e5d9257d5abb9e81c4567fe99e0c9c1
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-01-05 15:34:22 +0900

    Add demo_sd_generate_image_to_image_inpaint_xl_v1

--- demo_bedrock_stable_diffusion_xl_v1.py
+++ demo_bedrock_stable_diffusion_xl_v1.py
@@ -37,6 +37,8 @@ def run_demo(session):
     demo_sd_generate_image_to_image_xl_v1(bedrock_runtime, model_id, reference_image, iconfig["text"].strip(), iconfig["negative"], iconfig["style"], iconfig["scale"])
 
 
+    #demo_sd_generate_image_to_image_inpaint_xl_v1(bedrock_runtime, model_id, reference_image, "Add Sun", iconfig["negative"], iconfig["style"], iconfig["scale"])
+
 ####################
 
 def demo_sd_generate_image_xl_v1(bedrock_runtime, model_id, prompt, negative_prompts, style_preset="comic-book", cfg_scale = 10):
@@ -183,6 +185,104 @@ def demo_sd_generate_image_to_image_xl_v1(bedrock_runtime, model_id, reference_i
             "steps": config["steps"],
             #"style_preset": config["style_preset"]
             "init_image": input_image_b64,
+        }
+    )
+
+    #print(body)
+
+    # 
+    print("Generating Image ...")
+    response = bedrock_runtime.invoke_model(body=body, modelId=model_id)
+
+    # 
+    response_body = json.loads(response.get("body").read())
+    response_image = base64_to_image(response_body["artifacts"][0].get("base64"))
+
+    # 
+    response_image.save(OUTPUT_IMG_PATH)
+    # 
+    with open("{}.json".format(OUTPUT_IMG_PATH), "w") as f:
+        json.dump(config, f, ensure_ascii = False)
+
+    print("Complete")
+
+
+
+####################
+
+def demo_sd_generate_image_to_image_inpaint_xl_v1(bedrock_runtime, model_id, reference_imgage_filename, prompt, negative_prompts, style_preset="comic-book", cfg_scale = 10):
+
+    print(f"Call demo_sd_generate_image_to_image_inpaint_xl_v1 | style_preset={style_preset} | cfg_scale={cfg_scale} | reference_imgage_filename={reference_imgage_filename}")
+
+    print(f"PROMPT: {prompt}")
+    print(f"NEG_PROMPT: {negative_prompts}")
+
+    ####
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    file_extension = ".png"
+
+    file_name, file_extension = os.path.splitext(reference_imgage_filename)
+    INPUT_IMG_PATH = os.path.join(ROOT_DIR, "stable-diffusion/v1/in/{}".format(reference_imgage_filename))
+    print("INPUT_IMG_PATH: " + INPUT_IMG_PATH)
+
+    INPUT_IMG_MASK_PATH = os.path.join(ROOT_DIR, "stable-diffusion/v1/in/{}".format("black_mask.png"))
+    print("INPUT_IMG_MASK_PATH: " + INPUT_IMG_MASK_PATH)
+
+    OUTPUT_IMG_PATH = os.path.join(ROOT_DIR, "stable-diffusion/v1/out/{}{}".format(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"), file_extension))
+    print("OUTPUT_IMG_PATH: " + OUTPUT_IMG_PATH)
+
+    seed = random.randint(0, 4294967295)
+    steps = 50 #150 #30 #50
+    #cfg_scale = cfg_scale
+    start_schedule = 0.6
+    change_prompt = prompt
+    #negative_prompts = negative_prompts
+    #style_preset = style_preset
+    size = 1024
+
+    print(f"Loading Reference Image ... {INPUT_IMG_PATH}")
+    input_image_b64 = image_to_base64(Image.open(INPUT_IMG_PATH).resize((size, size)))
+
+    print(f"Loading Mask Image ... {INPUT_IMG_MASK_PATH}")
+    input_image_mask_b64 = image_to_base64(Image.open(INPUT_IMG_MASK_PATH).resize((size, size)))
+
+    # 
+    config = {
+        "filename": OUTPUT_IMG_PATH,
+        "seed": seed,
+        "change_prompt": change_prompt,
+        "steps": steps,
+        "cfg_scale": cfg_scale,
+        "start_schedule": start_schedule,
+        "style_preset": style_preset,
+        "size": size,
+        "negative_prompts": negative_prompts,
+        "input_image": INPUT_IMG_PATH
+    }
+
+    # 
+    body = json.dumps(
+        {
+            "text_prompts": (
+                #[{"text": config["change_prompt"]}]
+                #[{"text": config["change_prompt"], "weight": 1.0}]
+                #+ [{"text": negprompt, "weight": -1.0} for negprompt in negative_prompts]
+
+                [{"text": config["change_prompt"], "weight": 1.0}]
+                + [{"text": negprompt, "weight": -1.0} for negprompt in negative_prompts]
+            ),
+            "cfg_scale": config["cfg_scale"],
+            #"clip_guidance_preset"
+            #"height": "1024",
+            #"width": "1024",
+            "seed": config["seed"],
+            #"start_schedule": config["start_schedule"],
+            "steps": config["steps"],
+            #"style_preset": config["style_preset"]
+            "init_image": input_image_b64,
+            "mask_source": "MASK_IMAGE_BLACK",
+            "mask_image": input_image_mask_b64,
         }
     )
 
