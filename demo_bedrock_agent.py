ebe74841b86adcc5770e2f11aac00d63414591fa
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-02-28 22:13:28 +0900

    update agent

--- demo_bedrock_agent.py
+++ demo_bedrock_agent.py
@@ -13,7 +13,9 @@ def run_demo(session):
 
     bedrock_agent_runtime = session.client('bedrock-agent-runtime', region_name="us-east-1")
 
-    demo_invoke_bedrock_agent_runtime(bedrock_agent_runtime)
+    prompt = ""
+
+    demo_invoke_bedrock_agent_runtime(bedrock_agent_runtime, prompt)
 
 
 def demo_invoke_bedrock_agent_runtime(bedrock_agent_runtime, prompt="What is the first US ammendment?"):
@@ -23,20 +25,28 @@ def demo_invoke_bedrock_agent_runtime(bedrock_agent_runtime, prompt="What is the
     agent_id = config.bedrock_agent["agent_id"]
     agent_alias_id = config.bedrock_agent["agent_alias_id"]
 
+    print(f"agent_id={agent_id} agent_alias_id={agent_alias_id}")
+
     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/invoke_agent.html
     response = bedrock_agent_runtime.invoke_agent(
         agentId=agent_id, 
-        agentAliasId=agent_alias_id, 
-        sessionId=str(uuid.uuid4()), 
+        agentAliasId=agent_alias_id, # Use TSTALIASID as the agentAliasId to invoke the draft version of your agent.
+        sessionId=str(uuid.uuid4()),  # you continue an existing session with the agent if the value you set for the idle session timeout hasn't been exceeded.
         inputText=prompt, 
-        enableTrace=True, endSession=False)
+        enableTrace=False, 
+        endSession=False  # true to end the session with the agent.
+    )
     
     print(f"Answer: {response}")
 
     text = ""
     event_stream = response['completion']
     for event in event_stream:        
+        print(f"event={event}")
         if 'chunk' in event:
             text += event['chunk']['bytes'].decode("utf-8")
-            print(text)
-
+            print(event)
+        #else:
+            #print(event)
+    print(text)
+    print("End")
