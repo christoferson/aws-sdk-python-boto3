57325c0fbfd2a67ff65a522ad402ebdba572b71b
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-06-17 23:55:14 +0900

    chat with file

--- demo_bedrock_chat_with_file.py
+++ demo_bedrock_chat_with_file.py
@@ -4,108 +4,91 @@ import config
 import numpy as np
 import cmn_utils
 import logging
+import base64
 
 from botocore.exceptions import ClientError
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+# https://aws.amazon.com/jp/blogs/machine-learning/knowledge-bases-in-amazon-bedrock-now-simplifies-asking-questions-on-a-single-document/
+# https://aws.amazon.com/jp/blogs/machine-learning/knowledge-bases-in-amazon-bedrock-now-simplifies-asking-questions-on-a-single-document/
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/retrieve_and_generate.html
+# The supported file formats are PDF, MD (Markdown), TXT, DOCX, HTML, CSV, XLS, and XLSX. 
+# Make that the file size does not exceed 10 MB and contains no more than 20,000 tokens. 
+
+
 def run_demo(session):
 
     bedrock = session.client('bedrock')
 
     bedrock_runtime = session.client('bedrock-runtime', region_name="us-east-1")
+    bedrock_agent_runtime = session.client('bedrock-agent-runtime', region_name="us-east-1")
+
+
+    file_base64 = None
+    with open("data/movies.csv", "rb") as file:
+        file_base64 = base64.b64encode(file.read())
+    print(len(file_base64))
 
     model_id = 'anthropic.claude-3-sonnet-20240229-v1:0'
-    demo_stream_invoke_model_anthropic_claude_v3(bedrock_runtime, model_id, "Give me a trivia about pluto")
+    uploaded_file_type = "text/plain"
+    uploaded_file_bytes = file_base64
+    uploaded_file_name = "movies.csv"
+    prompt = """
+    What is the highest grossing film?
+    """
+    session_id = demo_stream_invoke_model_anthropic_claude_v3(bedrock_agent_runtime, model_id, prompt,
+        uploaded_file_type, uploaded_file_bytes, uploaded_file_name, current_session_id=None)
+    prompt = "When was that movie aired?"
+    session_id = demo_stream_invoke_model_anthropic_claude_v3(bedrock_agent_runtime, model_id, prompt,
+        uploaded_file_type, uploaded_file_bytes, uploaded_file_name, current_session_id=session_id)
 
-def demo_stream_invoke_model_anthropic_claude_v3(bedrock_runtime, model_id, input_text):
+def demo_stream_invoke_model_anthropic_claude_v3(bedrock_agent_runtime, model_id, input_text,
+        uploaded_file_type,
+        uploaded_file_bytes,
+        uploaded_file_name, current_session_id) -> str:
+    
+    session_id = None
 
     try:
 
-        system_prompt = "You are a helpful assistant."
-        max_tokens = 1000
+        params = {
+                "input": {"text": input_text},
+                "retrieveAndGenerateConfiguration": {
+                    "type": "EXTERNAL_SOURCES",
+                    "externalSourcesConfiguration": {
+                        "modelArn": model_id,
+                        "sources": [
+                            {
+                                "sourceType": "BYTE_CONTENT",
+                                "byteContent": {
+                                    "contentType": uploaded_file_type,
+                                    "data": uploaded_file_bytes,
+                                    "identifier": uploaded_file_name,
+                                },
+                            }
+                        ],
+                    },
+                },
+            }
+        
+        if current_session_id != None:
+            params["sessionId"] = current_session_id
+        
+        #body = json.dumps(params)
+        #print(body)
+        response = bedrock_agent_runtime.retrieve_and_generate(**params)
 
-        # Prompt with user turn only.
-        user_message =  {"role": "user", "content": input_text}
-        messages = [user_message]
+        print(response["output"]["text"])
+        #print(response["citations"])
 
-        #response = generate_message(bedrock_runtime, model_id, system_prompt, messages, max_tokens)
-        #print("User turn only.")
-        #print(json.dumps(response, indent=4))
-
-        # Prompt with both user turn and prefilled assistant response.
-        #Anthropic Claude continues by using the prefilled assistant text.
-        #assistant_message =  {"role": "assistant", "content": "<emoji>"}
-        #messages = [user_message, assistant_message]
-        #response = generate_message(bedrock_runtime, model_id,system_prompt, messages, max_tokens)
-        body = json.dumps({
-                "anthropic_version": "bedrock-2023-05-31",
-                "max_tokens": max_tokens,
-                "system": system_prompt,
-                "messages": [
-                    {
-                        "role": "user", # Valid values are user and assistant. 
-                        "content": [
-                            {"type": "text", "text": input_text}, #Valid values are image and text. 
-                            #{"type": "image", 
-                                #"source": {
-                                    #"type": "base64",
-                                    #"media_type": "image/jpeg", 
-                                    #"data": encoded_string.decode('utf-8')
-                                #}
-                            #}
-                        ]
-                    }
-                ],
-                "temperature": 0.1,
-                "top_p": 0.999,
-                "top_k": 0, #100,000,000
-                #"stop_sequences": [""]
-            })
-        print(body)
-        response = bedrock_runtime.invoke_model_with_response_stream(body=body, modelId=model_id)
-        #response_body = json.loads(response.get('body').read())
-        #response = response_body
-        print(response)
-   
-
-        for event in response.get("body"):
-            chunk = json.loads(event["chunk"]["bytes"])
-
-            #message_start {'type': 'message_start', 'message': {'id': 'msg_01FfyBMnL4zddyMP5YNkYjhi', 'type': 'message', 'role': 'assistant', 'content': [], 'model': 'claude-3-sonnet-28k-20240229', 'stop_reason': None, 'stop_sequence': None, 'usage': {'input_tokens': 21, 'output_tokens': 1}}}
-
-            if chunk['type'] == 'message_start':
-                print(f"\Input Tokens: {chunk['message']['usage']['input_tokens']}")
-
-            elif chunk['type'] == 'message_delta':
-                print(f"\nStop reason: {chunk['delta']['stop_reason']}")
-                print(f"Stop sequence: {chunk['delta']['stop_sequence']}")
-
-                #print(f"Input tokens: {chunk['usage']['input_tokens']}")
-                print(f"Output tokens: {chunk['usage']['output_tokens']}")
-
-            elif chunk['type'] == 'content_block_delta':
-                if chunk['delta']['type'] == 'text_delta':
-                    print(chunk['delta']['text'], end="")
-
-            # message_stop {'type': 'message_stop', 'amazon-bedrock-invocationMetrics': {'inputTokenCount': 21, 'outputTokenCount': 212, 'invocationLatency': 4296, 'firstByteLatency': 742}}
-            elif chunk['type'] == 'message_stop':
-                #if chunk['delta']['type'] == 'text_delta':
-                metrics = chunk['amazon-bedrock-invocationMetrics']
-                print(f"inputTokenCount: {metrics['inputTokenCount']}")
-                print(f"outputTokenCount: {metrics['outputTokenCount']}")
-                print(f"invocationLatency: {metrics['invocationLatency']}")
-                print(f"firstByteLatency: {metrics['firstByteLatency']}")
-
-            
-            elif chunk['type'] == 'content_block_start':
-                pass
-
-            else:
-                print(f"\n{chunk['type']} {chunk}\n")
+        session_id = response["sessionId"]
+        print(session_id)
 
     except ClientError as err:
         message = err.response["Error"]["Message"]
         logger.error("A client error occurred: %s", message)
         print("A client error occured: " + format(message))
+
+    return session_id
