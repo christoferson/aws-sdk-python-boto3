16f70473ce60400a3fd931d6057050b6cdb479bf
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-07-09 20:25:48 +0900

    tool stream

--- demo_bedrock_converse_api_with_tool_stream.py
+++ demo_bedrock_converse_api_with_tool_stream.py
@@ -12,6 +12,8 @@ import copy
 
 from botocore.exceptions import ClientError
 
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
 
 class StationNotFoundError(Exception):
     """Raised when a radio station isn't found."""
@@ -21,19 +23,20 @@ class StationNotFoundError(Exception):
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-
-
 def run_demo(session):
 
-    bedrock = session.client('bedrock')
+    
+    #bedrock = session.client('bedrock')
+
+    demo_tool_stream(session, "What is the most popular song on WZPZ?")
+
+
+def demo_tool_stream(session, input_text):
+
 
     bedrock_runtime = session.client('bedrock-runtime', region_name="us-east-1")
 
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-
-
     model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
-    input_text = "What is the most popular song on WZPZ?"
 
     tool_config = {
         "tools": [
@@ -64,10 +67,7 @@ def run_demo(session):
     try:
         print(f"Question: {input_text}")
 
-        message_user = {
-            "role": "user",
-            "content": [{"text": input_text}]
-        }
+        message_user = { "role": "user", "content": [{"text": input_text}] }
 
         messages = [message_user]
 
@@ -75,6 +75,9 @@ def run_demo(session):
         print(tool_invocation)
 
         if tool_invocation['tool_name'] != None:
+
+            tool_name = tool_invocation['tool_name']
+            tool_args_json = json.loads(tool_invocation['tool_arguments'])
 
             tool_request_message = {
                 "role": "assistant",
@@ -88,6 +91,10 @@ def run_demo(session):
                     }
                 ]
             }
+            song = ""
+            artist = ""
+            if tool_name == "top_song":
+                song, artist = get_top_song(tool_args_json['sign'])
 
             tool_result_message = {
                 "role": "user",
@@ -95,7 +102,7 @@ def run_demo(session):
                     {
                         "toolResult": {
                                 "toolUseId": tool_invocation['tool_use_id'],
-                                "content": [{"json": {"song": "Mysong", "artist": 'My Artist'}}]
+                                "content": [{"json": {"song": song, "artist": artist}}]
                                 #"status": 'error',
                             }
 
@@ -115,28 +122,6 @@ def run_demo(session):
     else:
         print(
             f"Finished generating text with model {model_id}.")
-
-
-def get_top_song(call_sign):
-    """Returns the most popular song for the requested station.
-    Args:
-        call_sign (str): The call sign for the station for which you want
-        the most popular song.
-
-    Returns:
-        response (json): The most popular song and artist.
-    """
-
-    song = ""
-    artist = ""
-    if call_sign == 'WZPZ':
-        song = "Elemental Hotel"
-        artist = "8 Storey Hike"
-
-    else:
-        raise StationNotFoundError(f"Station {call_sign} not found.")
-
-    return song, artist
 
 
 def generate_text(bedrock_client, model_id, tool_config, messages):
@@ -218,3 +203,27 @@ def generate_text(bedrock_client, model_id, tool_config, messages):
                         f"Latency: {metadata['metrics']['latencyMs']} milliseconds")
 
     return tool_invocation
+
+
+
+
+def get_top_song(call_sign):
+    """Returns the most popular song for the requested station.
+    Args:
+        call_sign (str): The call sign for the station for which you want
+        the most popular song.
+
+    Returns:
+        response (json): The most popular song and artist.
+    """
+
+    song = ""
+    artist = ""
+    if call_sign == 'WZPZ':
+        song = "Elemental Hotel"
+        artist = "8 Storey Hike"
+
+    else:
+        raise StationNotFoundError(f"Station {call_sign} not found.")
+
+    return song, artist
